eb912c911164ccbe727c50a4c39166fdae50e8f4
Nitin Bhardwaj <nitin.bhardwaj@transorg.com>
2023-01-30 21:55:47 +0530

    update commit

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-eee="nitinbhardwaj04@gmail.com"
+ee="nitinbhardwaj04@gmail.com"
 def check_email(email):
     l=['nitinbhardwaj00','nitinbhardwaj01','nitinbhardwaj02']
     email=email.split("@")[0]
@@ -12,4 +12,4 @@ def check_email(email):
             print("No")
 
 
-print(check_email(eee))
+print(check_email(ee))
