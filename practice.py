2187ba1527f23c22637b5d6b3cd879749665c72b
Nitin Bhardwaj <nitin.bhardwaj@transorg.com>
2023-01-30 20:52:42 +0530

    this is email verification

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
-a=int(input())
-b=int(input())
-print(a*b+a)
+e="nitinbhardwaj04@gmail.com"
+def check_email(email):
+    l=['nitinbhardwaj00','nitinbhardwaj01','nitinbhardwaj02']
+    email=email.split("@")[0]
+    if email in l:
+        print("already exist")
+    else:
+        if email not in l and email.isalnum():
+            l.append(email)
+            print(l,"yes")
+        else:
+            print("No")
+
+
+print(check_email(e))
